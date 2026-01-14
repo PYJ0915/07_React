5df4e04993a7eab2094523c20605e09a245d26bd
PYJ0915 <qkrdbwls3542@gmail.com>
2026-01-14 17:47:42 +0900

    0114 수업끝

--- board-project-admin/src/App.jsx
+++ board-project-admin/src/App.jsx
@@ -3,6 +3,8 @@ import { AuthContext, AuthProvider } from './components/AuthContext'
 import DashBoard from './components/DashBoard'
 import Login from './components/Login'
 import './css/App.css'
+import { BrowserRouter } from 'react-router-dom';
+
 
 // 컴포넌트를 분리하여 하위 컴포넌트에서 useContext 사용
 function App() {
@@ -14,7 +16,7 @@ function App() {
 }
 
 function AppComponent() {
-  
+
   const { user } = useContext(AuthContext);
   // 로그인을 했다면 DashBoard 랜더링
   // 로그인을 안했다면 Login 렌더링
@@ -25,17 +27,21 @@ function AppComponent() {
 
   return (
     <>
-      { user ? 
-      (
-        <div className='body-container'>
-          <DashBoard />
-        </div>
-      )
-      : (
-        <div className='login-section'>
-          <Login />
-        </div>
-      )}
+      {user ?
+        (
+          <div className='body-container'>
+            {/* BrowserRouter : React 앱에서 URL 경로에 따라 컴포넌트를 부여줄 수 있게 해주는 라우팅 컨테이너
+              (라우팅이 적용될 부분의 최상위 부모 컴포넌트) => Route, Link, NavLink, useNavigate() 등 같은 라우팅 관련 기능 사용 가능 */}
+            <BrowserRouter>
+              <DashBoard />
+            </BrowserRouter>
+          </div>
+        )
+        : (
+          <div className='login-section'>
+            <Login />
+          </div>
+        )}
     </>
   )
 }
