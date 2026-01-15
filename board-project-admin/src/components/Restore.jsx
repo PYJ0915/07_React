eb6cf0da1bd2e38798ce72c4be50f42231c76983
PYJ0915 <qkrdbwls3542@gmail.com>
2026-01-15 12:35:06 +0900

    0115 수업 끝

--- board-project-admin/src/components/Restore.jsx
+++ board-project-admin/src/components/Restore.jsx
@@ -1,21 +1,142 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosApi } from "../api/axiosAPI";
 
 export default function Restore() {
-  return (
-    <div className="menu-box">
-      <section className="section-border">
-        <h2>탈퇴 회원 복구</h2>
+  const [withdrawnMembers, setwithdrawnMembers] = useState(null); // 탈퇴 회원 목록
 
-        <h3>탈퇴한 회원 목록</h3>
+  // 삭제 게시글 목록 상태
+  const [deleteBoards, setDeleteBoards] = useState(null);
 
-      </section>
+  const [isLoading, setIsLoading] = useState(true);
 
-      <section className="section-border">
-        <h2>삭제 게시글 복구</h2>
+  // 탈퇴한 회원 목록 조회용 함수
+  const getWithdrawnMemberList = async () => {
+    try {
 
-        <h3>삭제된 게시글 목록</h3>
+      const resp = await axiosApi.get("/admin/withdrawnMemberList");
 
-      </section>
-    </div>
-  );
+      if (resp.status == 200) {
+        setwithdrawnMembers(resp.data);
+      }
+
+    } catch (error) {
+      console.error("탈퇴 회원 목록 조회 중 에러 발생 : ", error);
+    }
+  }
+
+  // 탈퇴한 회원 복구 요청 함수
+  const restoreMember = async (member) => {
+    if(!window.confirm(member.memberNickname + "님을 탈퇴 복구 시키시겠습니까?")) return;
+
+    try {
+      
+      const resp = await axiosApi.put("/admin/restoreMember", { memberNo : member.memberNo })
+
+      if(resp.status == 200) {
+        alert("복구 완료!");
+        getWithdrawnMemberList();
+      }
+      
+    } catch (error) {
+      console.error("탈퇴 회원 복구 중 에러 발생 : ", error);
+    }
+  }
+
+  // 삭제된 게시글 목록 조회용 함수
+  const selectDeleteBoardList = async () => {
+    try {
+
+      const resp = await axiosApi.get("/admin/deleteBoardList");
+
+      if (resp.status == 200) {
+       setDeleteBoards(resp.data);
+      }
+
+    } catch (error) {
+      console.error("삭제된 게시글 목록 조회 중 에러 발생 : ", error);
+    }
+  }
+
+  // 삭제된 게시글 복구 요청 함수
+  const restoreBoard = async (board) => {
+    if(!confirm(board.boardNo + "번 게시글을 복구하시겠습니까?")) return;
+
+    try {
+      
+      const resp = await axiosApi.put("/admin/restoreBoard", { boardNo : board.boardNo })
+
+      if(resp.status == 200) {
+        alert("복구 완료!");
+        selectDeleteBoardList();
+      }
+
+    } catch (error) {
+      console.error("삭제된 게시글 복구 중 에러 발생 : ", error);
+    }
+  }
+
+  // Restore 컴포넌트가 첫 마운트될 때 실행
+  useEffect(() => {
+    getWithdrawnMemberList();
+    selectDeleteBoardList();
+  }, [])
+
+  useEffect(() => {
+
+    if (withdrawnMembers != null && deleteBoards != null) {
+      setIsLoading(false);
+    }
+
+  }, [withdrawnMembers, deleteBoards]);
+
+  if (isLoading) {
+
+    return <h1>Loading...</h1>
+
+  } else {
+
+    return (
+      <div className="menu-box">
+        <section className="section-border">
+          <h2>탈퇴 회원 복구</h2>
+
+          <h3>탈퇴한 회원 목록</h3>
+
+          {withdrawnMembers.length === 0 ? (<p>탈퇴한 회원이 없습니다.</p>) : (
+            withdrawnMembers.map((member, index)=>{
+              return (
+                <ul className="ul-board" key={index}>
+                  <li>회원 번호 : {member.memberNo}</li>
+                  <li>회원 이메일 : {member.memberEmail}</li>
+                  <li>회원 닉네임 : {member.memberNickname}</li>
+                  <button className="retoreBtn" onClick={() => restoreMember(member)}>복구</button>
+                </ul>
+              )
+            })
+          )}
+
+        </section>
+
+        <section className="section-border">
+          <h2>삭제 게시글 복구</h2>
+
+          <h3>삭제된 게시글 목록</h3>
+
+          {deleteBoards.length === 0 ? (<p>삭제된 게시글이 존재하지 않습니다.</p>) : (
+            deleteBoards.map((board, index) => (
+              <ul className="ul-board" key={index}>
+                <li>게시글 번호 : {board.boardNo}</li>
+                <li>게시글 제목 : {board.boardTitle}</li>
+                <li>게시글 작성일 : {board.boardWriteDate}</li>
+                <button className="restoreBtn" onClick={() => restoreBoard(board)}>복구</button>
+              </ul>
+            ))
+          )}
+
+        </section>
+      </div>
+    );
+
+  }
+
 }
--- board-project-admin/src/components/Statistics.jsx
+++ board-project-admin/src/components/Statistics.jsx
@@ -1,19 +1,114 @@
-import React  from "react";
+import React, { useEffect, useState } from "react";
+import { axiosApi } from "../api/axiosAPI";
 
 export default function Statistics() {
-  return (
-    <div>
-      <section className="statistics-section">
-        <h2>가장 조회수 많은 게시글</h2>
-      </section>
 
-      <section className="statistics-section">
-        <h2>가장 좋아요 많은 게시글</h2>
-      </section>
+  const [readCountData, setReadCountData] = useState(null);
+  const [likeCountData, setLikeCountData] = useState(null);
+  const [commentCountData, setCommentCountData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-      <section className="statistics-section">
-        <h2>가장 댓글 많은 게시글</h2>
-      </section>
-    </div>
-  );
+  // 가장 조회수 많은 게시글
+  const getMaxReadCount = async () => {
+    // 직접 작성해보기
+
+    try {
+
+      const resp = await axiosApi.get("/admin/maxReadCount");
+
+      if (resp.status == 200) {
+        // 조회수 가장 많은 게시글 상태에 세팅
+        setReadCountData(resp.data);
+      }
+
+    } catch (error) {
+      console.error("최다 조회수 게시글 조회 중 예외 발생", error)
+    }
+  }
+
+  // 가장 좋아요 많은 게시글
+  const getMaxLikeCount = async () => {
+    // 직접 작성해보기
+
+    try {
+
+      const resp = await axiosApi.get("/admin/maxLikeCount");
+
+      if (resp.status == 200) {
+        setLikeCountData(resp.data)
+      }
+
+    } catch (error) {
+      console.error("최다 좋아요 게시글 조회 중 예외 발생", error)
+    }
+
+
+  }
+
+  // 가장 댓글 많은 게시글
+  const getMaxCommentCount = async () => {
+    // 직접 작성해보기
+
+    try {
+
+      const resp = await axiosApi.get("/admin/maxCommentCount");
+
+      if (resp.status == 200) {
+        setCommentCountData(resp.data)
+      }
+
+    } catch (error) {
+      console.error("최다 댓글 게시글 조회 중 예외 발생", error)
+    }
+
+
+  }
+
+  // 컴포넌트가 처음 마운트 될 때 1번 실행
+  useEffect(() => {
+    getMaxReadCount();
+    getMaxLikeCount();
+    getMaxCommentCount();
+  }, []); // 의존성 배열이 비어있기 때문에 1번만 실행됨.
+
+  // readCountData, likeCountData, commentCountData
+  useEffect(() => {
+
+    if (readCountData != null && likeCountData != null && commentCountData != null) {
+      setIsLoading(false);
+    }
+  }, [readCountData, likeCountData, commentCountData]);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  } else {
+
+    return (
+      <div>
+        <section className="statistics-section">
+          <h2>가장 조회수 많은 게시글</h2>
+          <p>게시판 종류 : {readCountData.boardName} </p>
+          <p>게시판 번호/제목 : No.{readCountData.boardNo} / {readCountData.boardTitle}</p>
+          <p>게시글 조회수 : {readCountData.readCount}</p>
+          <p>작성자 닉네임 : {readCountData.memberNickname}</p>
+        </section>
+
+        <section className="statistics-section">
+          <h2>가장 좋아요 많은 게시글</h2>
+          <p>게시판 종류 : {likeCountData.boardName} </p>
+          <p>게시판 번호/제목 : No.{likeCountData.boardNo} / {likeCountData.boardTitle}</p>
+          <p>좋아요 수 : {likeCountData.likeCount}</p>
+          <p>작성자 닉네임 : {likeCountData.memberNickname}</p>
+        </section>
+
+        <section className="statistics-section">
+          <h2>가장 댓글 많은 게시글</h2>
+          <p>게시판 종류 : {commentCountData.boardName} </p>
+          <p>게시판 번호/제목 : No.{commentCountData.boardNo} / {commentCountData.boardTitle}</p>
+          <p>댓글 수 : {commentCountData.commentCount}</p>
+          <p>작성자 닉네임 : {commentCountData.memberNickname}</p>
+        </section>
+      </div>
+    );
+  }
 }
